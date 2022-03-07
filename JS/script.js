5ee3c5a5ebc6718c3a0fec23f29a1c8168cdce6b
Dindihub <sadindi03@gmail.com>
2022-03-07 08:42:30 +0300

    Revise functions

--- JS/script.js
+++ JS/script.js
@@ -13,8 +13,7 @@ const generateAkanName = () => {
 }
 
 function isDateValid() {
-    if (
-        dayOfBirth <= 0 ||
+    if (dayOfBirth <= 0 ||
         dayOfBirth > 31 ||
         monthOfBirth <= 0 ||
         monthOfBirth > 12
@@ -24,32 +23,3 @@ function isDateValid() {
 
     return true;
 }
-
-
-// let day = document.getElementById("day");
-// let month = document.getElementById("month");
-// let year = document.getElementById("year");
-// let btn = document.getElementById("btn");
-
-// function Day() {
-//     let CC = year.substring(0, 2);
-//     let YY = year.substring(2);
-//     let MM = month.value;
-//     let DD = date.value;
-//     let d = ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7)
-//     console.log(d)
-// }
-
-// function generateName() {
-//     let gender = document.getElementById(gender);
-//     let index = Day()
-//     let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-//     let male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-//     if (gender.value == "female") {
-//         console.log(female[index])
-//     } else {
-//         console.log(male[index])
-//     }
-// }
-// btn.addEventListener("click"), (e) => e.preventDefault();
-// console.log("click here")
